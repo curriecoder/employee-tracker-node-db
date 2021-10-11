2ac8a808efe07c0a5fb194913b983bee163b232f
curriecoder <andrew.j.currie0@gmail.com>
2021-10-11 11:48:56 -0400

    added queries and .env files, started server.js build

---
+++ server.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.NAME,
+  },
+  console.log(`Connected to the employee_db database.`)
+);
